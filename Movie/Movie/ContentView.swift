6f4f4af5a51bc6929c7e266ec3e82d6bb04ed65b
ALvaro Javier Susa Hernandez <susancudo11@gmail.com>
2021-03-27 17:36:26 -0500

    Se carga el proyecto inicial

---
+++ Movie/Movie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie
+//
+//  Created by Javier Susa on 27/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
