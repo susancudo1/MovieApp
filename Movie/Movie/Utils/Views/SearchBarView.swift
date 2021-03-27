8750b5de073c0f2e5617a4f76e017d57c700e4c6
ALvaro Javier Susa Hernandez <susancudo11@gmail.com>
2021-03-27 18:25:23 -0500

    Creacion de la barra de busqueda

---
+++ Movie/Movie/Utils/Views/SearchBarView.swift
@@ -0,0 +1,69 @@
+//
+//  SearchBarView.swift
+//  Movie
+//
+//  Created by Javier Susa on 27/03/21.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    @Binding var text: String
+    @State private var isEditing = false
+
+    var body: some View {
+        HStack {
+
+            TextField("search_bar_view_placeholder_input_search".localized, text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+                            }, label: {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            })
+                        }
+                    }
+                )
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    self.text = ""
+                    // Dismiss the keyboard
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                                    to: nil,
+                                                    from: nil,
+                                                    for: nil)
+                }, label: {
+                    Text("search_bar_view_cancel_button".localized)
+                })
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView(text: .constant(""))
+    }
+}
