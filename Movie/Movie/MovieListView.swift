b2979c9d125372c79292124ef4da14950ce3aa66
ALvaro Javier Susa Hernandez <susancudo11@gmail.com>
2021-03-27 18:29:08 -0500

    Implementacion de la vista del listado de peliculas aun sin datos

---
+++ Movie/Movie/MovieListView.swift
@@ -0,0 +1,41 @@
+//
+//  MovieListView.swift
+//  Movie
+//
+//  Created by Javier Susa on 27/03/21.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+
+    @State private var searchText = ""
+    @State private var resultModel: ResultModel?
+    var body: some View {
+        NavigationView {
+            VStack {
+                SearchBarView(text: $searchText)
+                    .padding(.top)
+                if resultModel != nil {
+                    List((resultModel?.results.filter({ searchText.isEmpty ?
+                                                        true :
+                                                        $0.title.lowercased().contains(searchText.lowercased()) }))!,
+                         id: \.iden) { movie in
+                        ZStack {
+                            CellMovieView(title: movie.title,
+                                          imageData: movie.imageData)
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .navigationBarTitle("title_app".localized, displayMode: .inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView()
+    }
+}
